a50219738c3b9a884cf72a427dd6a4414fa8bc3e
Rasmus Bremholm <rasmus.brem@gmail.com>
2024-07-04 14:36:29 +0200

    adjustments

--- app/page.js
+++ app/page.js
@@ -2,9 +2,26 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      
-    </main>
-  );
+	return (
+		<>
+			<main className={styles.main}>
+				<h1>
+					Grid Example: Lorem, ipsum dolor sit amet consectetur adipisicing
+					elit.
+				</h1>
+			</main>
+			<p className={styles.paragraph1}>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum quasi
+				placeat dignissimos vitae voluptate quos incidunt id voluptas, itaque
+				earum nobis repudiandae natus cumque suscipit maiores asperiores
+				exercitationem voluptatibus? Perspiciatis?
+			</p>
+			<p className={styles.paragraph2}>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum quasi
+				placeat dignissimos vitae voluptate quos incidunt id voluptas, itaque
+				earum nobis repudiandae natus cumque suscipit maiores asperiores
+				exercitationem voluptatibus? Perspiciatis?
+			</p>
+		</>
+	);
 }
